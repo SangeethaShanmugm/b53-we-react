fc7283c1d4cb9b17cb732f581e383e27c494d87a
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-02-04 13:08:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
+  //JS starts
+  // const name = "Raja";
+  // const name1 = "Dinesh";
+  const people = ["Raja", "Dinesh", "Babu"]; //Array of string
+  const user = [
+    {
+      name: "Raja",
+      age: "20",
+    },
+    {
+      name: "Dinesh",
+      age: "22",
+    },
+    {
+      name: "Babu",
+      age: "25",
+    },
+  ];
+  //JS ends
+  //JSX starts
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.map((usr) => (
+        <Welcome nm={usr.name} ag={usr.age} />
+      ))}
+      {/* {people.map((person) => (
+        <Welcome name={person} />
+      ))} */}
+      {/* <Welcome name="Raja" age="20" />
+      <Welcome name="Dinesh" age="22" />
+      <Welcome name="Babu" age="25" /> */}
     </div>
+  );
+  //JSX ends
+}
+
+function Welcome({ nm, ag }) {
+  return (
+    <h1>
+      Hello Everyone by {nm} -{ag} 😆😆😆
+    </h1>
   );
 }
 
-export default App;
+//JSX  => Javascript XML
+//class => reserved keyword
+//{} => template syntax
+//custom component
+// 1. First letter should be capital
+// 2. It should return JSX
